e1604514b1c658cb5eb2c3a8ef1eebc8be3512e5
Yoann <yderneville@gmail.com>
2019-03-08 14:17:38 +0100

    Update
    Thread added

---
+++ ChenillardController/Chenillard.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace ChenillardConsole.ChenillardController
+{
+    class Chenillard
+    {
+
+        private Boolean ActiveChen;
+        public int speed;
+        
+        public Chenillard()
+        {
+            this.ActiveChen = false;
+            this.speed = 500;
+        }
+
+        public void startChenillard()
+        {
+            ThreadStart threadStart = new ThreadStart(GoChenillard);
+            Thread threadChen = new Thread(threadStart);
+            ActiveChen = true;
+
+            threadChen.Start();
+        }
+
+        public void GoChenillard()
+        {
+            while (ActiveChen == true)
+            {
+                Console.WriteLine("(1)");
+                Thread.Sleep(this.speed);
+                Console.WriteLine("(.)");
+                Console.WriteLine("(2)");
+                Thread.Sleep(this.speed);
+                Console.WriteLine("(.)");
+                Console.WriteLine("(3)");
+                Thread.Sleep(this.speed);
+                Console.WriteLine("(.)");
+                Console.WriteLine("(4)");
+                Thread.Sleep(this.speed);
+                Console.WriteLine("(.)");
+            }
+        }
+
+        public void stopChenillard()
+        {
+            this.ActiveChen = false;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using ChenillardConsole.ChenillardController;
+namespace ChenillardConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Chenillard Console 
+            int counter = 0;
+            int speedCmd = 500;
+            string command = null;
+            Boolean loopContinue = true;
+            Console.WriteLine($@"=============***>>>>>>>                            <<<<<<<<<<<******==========
+                    Welcome to the Chenillard console ");
+            Chenillard chenillardContol = new Chenillard();
+
+            while (true && loopContinue== true)
+            {
+                Console.WriteLine("Start the chenillard by pressing [V], stop chenillard with [C], select your speed with [S]. To quit, press [Q]. Actual speed : " + chenillardContol.speed + "ms");
+                command = Console.ReadLine();
+                switch (command)
+                {
+                    case "V":
+                        chenillardContol.startChenillard();
+                        break;
+                    case "C":
+                        chenillardContol.stopChenillard();
+                        break;
+                    case "S":
+                        Console.WriteLine("Choose your Speed : ");
+                        speedCmd = Convert.ToInt32(Console.ReadLine());
+                        chenillardContol.speed = speedCmd;
+                        break;
+                    case "Q":
+                        Console.WriteLine("Exiting..");
+                        loopContinue = false;
+                        break;
+                }
+
+
+                //Console.WriteLine("Start Chenillard");
+
+                //chenillardContol.startChenillard();
+            }
+
+            Console.WriteLine("End... ");
+            Console.ReadLine();
+        }
+
+    }
+}
